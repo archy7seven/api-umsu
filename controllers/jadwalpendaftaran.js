64f96547d31f2126a03baf5359ca2183f469b3c8
Muhammad Arizky <arizkymuhammad46@gmail.com>
2023-01-05 15:43:38 +0700

    Menambah controller JadwalPendaftaran

---
+++ controllers/jadwalpendaftaran.js
@@ -0,0 +1,39 @@
+const jadwalPendaftaran = {}
+const connection = require('../koneksipenmaru')
+const jdwl = require('../models/m_jadwal')
+
+jadwalPendaftaran.index = async (req, res) => {
+    try {
+        connection.query("SELECT * FROM r_periode LEFT JOIN r_tahun_aktif on r_tahun_aktif.tahunNama = r_periode.prdrTahun WHERE prdrJenisUjian = 'NonKedokteran' AND tahunAktif = 1 AND prdrStatusAktif = 1",
+            async function (error, rows, fields) {
+                if (error) {
+                    res.status(500).send({
+                        status: false,
+                        message: 'Data Tidak Ditemukan..!',
+                        data: null
+                    })
+                } else {
+                    const jadwalCBT = await jdwl.dataJadwalCBT(res)
+                    const jadwalPBT = await jdwl.dataJadwalPBT(res)
+                    const jadwalFK = await jdwl.dataJadwalFK(res)
+                    const jadwalPasca = await jdwl.dataJadwalPasca(res)
+                    res.send({
+                        status: true,
+                        message: 'Data Di Temukan..!',
+                        dataJadwalCBT: jadwalCBT,
+                        dataJadwalPBT: jadwalPBT,
+                        dataJadwalFK: jadwalFK,
+                        dataJadwalPasca: jadwalPasca
+                    })
+                }
+            })
+    } catch (error) {
+        res.status(500).send({
+            status: false,
+            message: 'Data Tidak Di Temukan..!',
+            data: null
+        })
+    }
+}
+
+module.exports = jadwalPendaftaran
